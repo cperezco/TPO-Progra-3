da3f3d8c1fdbe1fa619cc805525f93d05ba96688
Julian Mauro <150207298+JulianMauro@users.noreply.github.com>
2024-10-03 21:36:49 -0300

    Detailed Explanation, fixes and code proficiency improved

--- Clase 1/MaximoArray.java
+++ Clase 1/MaximoArray.java
@@ -1,10 +1,6 @@
 public class MaximoArray {
-    public static void main(String[] args) {
-        int[] numeros = {3, 7, 2, 9, 4, 5};
-        int maximo = encontrarMaximo(numeros);
-        System.out.println("El máximo valor en el array es: " + maximo);
-    }
 
+    // Encuentra el numero maximo dentro de un array de ints
     public static int encontrarMaximo(int[] array) {
         int maximo = array[0];
         for (int i = 1; i < array.length; i++) {
@@ -13,5 +9,11 @@ public class MaximoArray {
             }
         }
         return maximo;
+    }
+
+    public static void main(String[] args) {
+        int[] numeros = {3, 7, 2, 9, 4, 5, -1, 100, 200, 5, 0};
+        int maximo = encontrarMaximo(numeros);
+        System.out.println("El máximo valor en el array es: " + maximo);
     }
 }
