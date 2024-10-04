9c2f0fa9deb50c25cd0804ddee94dcb3a64d2270
Julian Mauro <150207298+JulianMauro@users.noreply.github.com>
2024-10-03 21:41:38 -0300

    Improvement of code. Names and style fixed. Comments and more.

--- Clase 2/QuickSort.java
+++ Clase 2/QuickSort.java
@@ -1,37 +1,44 @@
 public class QuickSort {
 
-    public static void quickSort(int[] arreglo, int bajo, int alto) {
-        if (bajo < alto) {
+    // Quicksort. Utilizamos recursividad para poder ordenar el array rapidamente. O(n·log n)
+    public static void quickSort(int[] arreglo, int bajo, int alto) 
+    {
+        if (bajo < alto) 
+        {
             int indiceParticion = particion(arreglo, bajo, alto);
             quickSort(arreglo, bajo, indiceParticion - 1);
             quickSort(arreglo, indiceParticion + 1, alto);
         }
     }
 
-    private static int particion(int[] arreglo, int bajo, int alto) {
+    private static int particion(int[] arreglo, int bajo, int alto) 
+    {
         int pivote = arreglo[alto];
         int i = (bajo - 1);
 
-        for (int j = bajo; j < alto; j++) {
+        for (int j = bajo; j < alto; j++) 
+        {
             if (arreglo[j] <= pivote) {
                 i++;
-                int temp = arreglo[i];
+                int pointer = arreglo[i];
                 arreglo[i] = arreglo[j];
-                arreglo[j] = temp;
+                arreglo[j] = pointer;
             }
         }
-        int temp = arreglo[i + 1];
+        int temporal = arreglo[i + 1];
         arreglo[i + 1] = arreglo[alto];
-        arreglo[alto] = temp;
+        arreglo[alto] = temporal;
 
         return i + 1;
     }
-    public static void main(String[] args) {
-        int[] arreglo = {50,6,3,8,5,2,51,4,1};
-        quickSort(arreglo, 0, arreglo.length - 1);
+    public static void main(String[] args) 
+    {
+        int[] arrayDesordenado = {50,6,3,8,5,2,51,4,1};
+        quickSort(arrayDesordenado, 0, arrayDesordenado.length - 1);
         System.out.print("Array ordenado: ");
-        for (int i = 0; i < arreglo.length; i++) {
-            System.out.print(arreglo[i] + " ");
+        for (int i = 0; i < arrayDesordenado.length; i++) 
+        {
+            System.out.print(arrayDesordenado[i] + " ");
         }
     }
 }
