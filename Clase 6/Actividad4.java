e3be356f8ad64eb2ae9ac4d70f8810536d95e910
Julian Mauro <150207298+JulianMauro@users.noreply.github.com>
2024-10-03 23:02:37 -0300

    Comments and clearance of code for better understanding!!!

--- Clase 6/Actividad4.java
+++ Clase 6/Actividad4.java
@@ -1,29 +1,35 @@
 import java.util.*;
 
-public class SeleccionPaquetes {
+public class SeleccionPaquetes 
+{
 
     // Fuerza Bruta: O(2^n) donde n es el numero de paquetes
-    public static int fuerzaBruta(int[] costos, int[] ganancias, int presupuesto) {
+    public static int fuerzaBruta(int[] costos, int[] ganancias, int presupuesto) 
+    {
         return auxFuerzaBruta(costos, ganancias, presupuesto, 0);
     }
 
-    private static int auxFuerzaBruta(int[] costos, int[] ganancias, int presupuesto, int index) {
-        if (index == costos.length) {
+    private static int auxFuerzaBruta(int[] costos, int[] ganancias, int presupuesto, int index) 
+    {
+        if (index == costos.length) 
+        {
             return 0;
         }
 
-        int opcion1 = auxFuerzaBruta(costos, ganancias, presupuesto, index + 1);
+        int opcion_1 = auxFuerzaBruta(costos, ganancias, presupuesto, index + 1);
 
-        int opcion2 = 0;
-        if (costos[index] <= presupuesto) {
-            opcion2 = ganancias[index] + auxFuerzaBruta(costos, ganancias, presupuesto - costos[index], index + 1);
+        int opcion_2 = 0;
+        if (costos[index] <= presupuesto) 
+        {
+            opcion_2 = ganancias[index] + auxFuerzaBruta(costos, ganancias, presupuesto - costos[index], index + 1);
         }
 
-        return Math.max(opcion1, opcion2);
+        return Math.max(opcion_1, opcion_2);
     }
 
     // Algoritmo Greedy: O(n log n) por la ordenación de los paquetes
-    public static int greedy(int[] costos, int[] ganancias, int presupuesto) {
+    public static int greedy(int[] costos, int[] ganancias, int presupuesto) 
+    {
         int n = costos.length;
         List<int[]> paquetes = new ArrayList<>();
 
@@ -35,8 +41,10 @@ public class SeleccionPaquetes {
 
         int gananciaTotal = 0;
 
-        for (int[] paquete : paquetes) {
-            if (paquete[0] <= presupuesto) {
+        for (int[] paquete : paquetes) 
+        {
+            if (paquete[0] <= presupuesto) 
+            {
                 gananciaTotal += paquete[1]; 
                 presupuesto -= paquete[0];   
             }
@@ -46,15 +54,20 @@ public class SeleccionPaquetes {
     }
 
     // Programación Dinámica: O(n * presupuesto) donde n es el número de paquetes y presupuesto es el límite
-    public static int programacionDinamica(int[] costos, int[] ganancias, int presupuesto) {
+    public static int programacionDinamica(int[] costos, int[] ganancias, int presupuesto) 
+    {
         int n = costos.length;
         int[][] dp = new int[n + 1][presupuesto + 1];
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= presupuesto; j++) {
-                if (costos[i - 1] <= j) {
+        for (int i = 1; i <= n; i++) 
+        {
+            for (int j = 0; j <= presupuesto; j++) 
+            {
+                if (costos[i - 1] <= j) 
+                {
                     dp[i][j] = Math.max(dp[i - 1][j], ganancias[i - 1] + dp[i - 1][j - costos[i - 1]]);
-                } else {
+                } else 
+                {
                     dp[i][j] = dp[i - 1][j];
                 }
             }
@@ -63,7 +76,8 @@ public class SeleccionPaquetes {
         return dp[n][presupuesto]; 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         int[] costos = {12, 20, 15, 25};
         int[] ganancias = {150, 200, 100, 300};
         int presupuesto = 35;
