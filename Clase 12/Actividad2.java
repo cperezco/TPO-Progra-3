f04b54d992c924554fb9725233ee0488e22d67ab
francooo <72412439+francolovera24@users.noreply.github.com>
2024-11-01 09:20:38 -0300

    Update and rename BFSGraph.java to Actividad2.java

--- Clase 12/Actividad2.java
+++ Clase 12/Actividad2.java
@@ -5,7 +5,6 @@ public class BFSGraph {
     private int numVertices;
     private LinkedList<Integer>[] adjacencyList;
 
-    // Constructor
     public BFSGraph(int vertices) {
         numVertices = vertices;
         adjacencyList = new LinkedList[vertices];
@@ -14,27 +13,21 @@ public class BFSGraph {
         }
     }
 
-    // Método para agregar una arista al grafo
     public void addEdge(int start, int end) {
         adjacencyList[start].add(end);
-        adjacencyList[end].add(start); // Grafo no dirigido
+        adjacencyList[end].add(start);
     }
-
-    // Método para realizar el recorrido BFS desde un nodo dado
     public void BFS(int startNode) {
         boolean[] visited = new boolean[numVertices];
         Queue<Integer> queue = new LinkedList<>();
 
-        // Marcar el nodo de inicio como visitado y encolarlo
         visited[startNode] = true;
         queue.add(startNode);
 
         while (!queue.isEmpty()) {
-            // Desencolar un vértice de la cola e imprimirlo
             int node = queue.poll();
             System.out.print(node + " ");
 
-            // Obtener todos los vértices adyacentes al nodo
             for (int neighbor : adjacencyList[node]) {
                 if (!visited[neighbor]) {
                     visited[neighbor] = true;
@@ -46,8 +39,6 @@ public class BFSGraph {
 
     public static void main(String[] args) {
         BFSGraph graph = new BFSGraph(9);
-
-        // Agregar aristas al grafo según la imagen
         graph.addEdge(0, 1);
         graph.addEdge(0, 2);
         graph.addEdge(0, 3);
